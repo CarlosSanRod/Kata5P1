13f4c9b521fdd3a1e88bede2e290bad4729aec34
Carlos Santana Rodríguez <carlos.santana.pelusa@gmail.com>
2020-12-06 13:06:04 +0000

    Kata 5 P1 versión 2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -11,18 +11,28 @@ public class Kata5 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
         Class.forName("org.sqlite.JDBC");
+        String sql = "CREATE TABLE IF NOT EXISTS probando (\n"
+                    + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                    + " direccion text NOT NULL);";
         
         try (
                 Connection connection = DriverManager.getConnection("jdbc:sqlite:data/us500.db");
                 
                 Statement statement = connection.createStatement()) {
-        
-                ResultSet result = statement.executeQuery("SELECT * FROM people");
+                    
+                    //Se crea la nueva tabla
+                    statement.execute(sql);
+                    System.out.println("Tabla creada");
+  
+                    //ResultSet result = statement.executeQuery("SELECT * FROM PEOPLE");
                 
+                /*
                 while (result.next()) {
                     String email = result.getString("email");
                     System.out.println(email);
                 }
+                */
+                    
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
