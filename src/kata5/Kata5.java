94b7105a060bf2682ae684b6e87e3ddd708624c1
Carlos Santana Rodríguez <carlos.santana.pelusa@gmail.com>
2020-12-06 23:06:19 +0000

    Kata 5 P1 versión 3

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -4,34 +4,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.ResultSet;
+import java.util.List;
+import kata5.persistance.MailListReader;
+import java.sql.PreparedStatement;
+import kata5.model.Mail;
 
 public class Kata5 {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
         Class.forName("org.sqlite.JDBC");
-        String sql = "CREATE TABLE IF NOT EXISTS probando (\n"
-                    + " id integer PRIMARY KEY AUTOINCREMENT,\n"
-                    + " direccion text NOT NULL);";
+        
+        String ruta = "emails.txt";
+        List<Mail> emails = MailListReader.read(ruta);
+       
         
         try (
                 Connection connection = DriverManager.getConnection("jdbc:sqlite:data/us500.db");
                 
                 Statement statement = connection.createStatement()) {
+            
+                    String createTable = "CREATE TABLE IF NOT EXISTS emails (\n"
+                            + "id integer PRIMARY KEY AUTOINCREMENT,\n"
+                            + "email text NOT NULL);";
                     
                     //Se crea la nueva tabla
-                    statement.execute(sql);
+                    statement.execute(createTable);
                     System.out.println("Tabla creada");
-  
-                    //ResultSet result = statement.executeQuery("SELECT * FROM PEOPLE");
-                
-                /*
-                while (result.next()) {
-                    String email = result.getString("email");
-                    System.out.println(email);
-                }
-                */
+                    
+                    String insertMail = "INSERT INTO emails(email) VALUES(?)";
+                    
+                    for (Mail email :  emails) {
+                        String direccion = email.getMail();
+                        
+                        try (PreparedStatement preparedStatement = connection.prepareStatement(insertMail)) {
+                            preparedStatement.setString(1, direccion);
+                            preparedStatement.executeUpdate();
+                        }
+                        
+                    }
                     
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
